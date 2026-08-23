e6f5d190af25f2d2ee141011461f7cbb31a73901
SKProCH <skproch@yandex.ru>
2026-08-23 15:26:35 +0300

    Enabled plugin by default

--- Jellyfin.Plugin.ReplayGain/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.ReplayGain/Configuration/PluginConfiguration.cs
@@ -3,7 +3,7 @@ using MediaBrowser.Model.Plugins;
 namespace Jellyfin.Plugin.ReplayGain.Configuration;
 
 public sealed class PluginConfiguration : BasePluginConfiguration {
-    public bool Enabled { get; set; } = false;
+    public bool Enabled { get; set; } = true;
 
     public bool PreserveDynamicRange { get; set; } = false;
 
