81c1ad69218f5f093092b3e4f34f7a9a77d1a489
SKProCH <skproch@yandex.ru>
2026-08-26 04:24:18 +0300

    Fixes the measurement method selector in gui

--- Jellyfin.Plugin.ReplayGain.Tests/Configuration/ConfigurationTests.cs
+++ Jellyfin.Plugin.ReplayGain.Tests/Configuration/ConfigurationTests.cs
@@ -5,7 +5,13 @@ namespace Jellyfin.Plugin.ReplayGain.Tests.Configuration;
 public sealed class ConfigurationTests {
     [Fact]
     public void PluginConfiguration_HasSafeDefaults() {
-        new PluginConfiguration().Enabled.Should().BeTrue();
-        new PluginConfiguration().MeasurementMethod.Should().Be(MeasurementMethod.Ebur128);
+        var configuration = new PluginConfiguration();
+
+        configuration.Enabled.Should().BeTrue();
+        configuration.MeasurementMethod.Should().Be(MeasurementMethod.Ebur128);
+        configuration.PreserveDynamicRange.Should().BeFalse();
+        configuration.LoudnormIntegratedLoudness.Should().Be(-16.0);
+        configuration.LoudnormTruePeak.Should().Be(-1.5);
+        configuration.LoudnormLoudnessRange.Should().Be(11.0);
     }
 }
